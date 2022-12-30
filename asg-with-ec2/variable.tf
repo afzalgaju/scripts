31f236ab7e3e16f15e1eaa2f688c77e3dc9a5892
arunnalladi <arun.kumar@mahiratechnology.com>
2022-12-30 19:18:07 +0530

    scripts

--- asg-with-ec2/variable.tf
+++ asg-with-ec2/variable.tf
@@ -3,10 +3,6 @@ variable "ami-id" {
   default     = "ami-074dc0a6f6c764218"
 }
 
-variable "aws-region" {
-  default = "ap-south-1a"
-}
-
 variable "instance-type" {
   type    = string
   default = "t2.micro"
